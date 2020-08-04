feb4d01fbee8f89c2f92229f10c02eaacdf06f96
GerardColman <gerard.colman@gmail.com>
2020-08-04 16:44:38 +0100

    Initial Push

---
+++ cmake-build-debug/CMakeFiles/DieRoller.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DieRoller.dir/main.cpp.obj"
+  "DieRoller.exe"
+  "DieRoller.exe.manifest"
+  "DieRoller.pdb"
+  "libDieRoller.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DieRoller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
