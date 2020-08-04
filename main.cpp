664621330c27482dcfc257d099f5065f9df2b3af
GerardColman <gerard.colman@gmail.com>
2020-08-04 17:10:18 +0100

    Added roller class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using namespace std;
+
+class Roller{
+    int main(int argc, char* argv[]){
+
+    }
+};
